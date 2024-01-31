216f202cce5db7015cbdbbb18db0dddc13d4926d
juwoong kim <wwong7247@gmail.com>
2024-01-31 09:11:48 +0900

    userState로 Counter 구현해보기

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from './section/001/Library';
-import NotificationList from './section/002/NotificationList';
+import Counter from './section/003/Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList/>
+    <Counter/>
   </React.StrictMode>
 );
 
---
+++ src/section/003/Counter.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+
+const Counter = () => {
+    const [value, setValue] = useState(0);
+
+    return (
+        <div>
+            <p>
+                카운터 값 <b>{value}</b>
+            </p>
+            <button onClick={() => setValue(prev => prev + 1)}>+1</button>
+            <button onClick={() => setValue(value - 1)}>-1</button>
+        </div>
+    );
+}
+
+export default Counter;
